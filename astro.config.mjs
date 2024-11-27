41a74b0ee204b3022608ebb9d62f4314f2e6f22d
Vangreen <pawelwangryn0@gmail.com>
2024-11-27 20:11:44 +0100

    docker image fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
-  output: "server"
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
